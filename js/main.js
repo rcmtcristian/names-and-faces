7403685b51cd70fd3799ac244565891153ca3c2a
rcmtcristian <rcmtcristian@gmail.com>
2022-10-14 18:34:17 -0400

    changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.querySelector("button").addEventListener("click", apiRequest);
 
 async function apiRequest() {
-  const rapperName = document.querySelector("input").value;
+  const personName = document.querySelector("input").value;
   try {
     const response = await fetch(
       `https://names-and-faces-nraivuy3w-rcmtcristian.vercel.app/api/${personName}`
